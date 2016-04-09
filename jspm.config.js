768f7ee050d3bdd6df3048d5195b45e47e0d9480
Piotr Witek <piotrek.witek@gmail.com>
2016-04-10 00:27:44 +0200

    updated react to 0.15

--- jspm.config.js
+++ jspm.config.js
@@ -31,8 +31,10 @@ SystemJS.config({
     "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
     "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
     "child_process": "github:jspm/nodelibs-child_process@0.2.0-alpha",
-    "clean-css": "npm:clean-css@3.4.10",
+    "clean-css": "npm:clean-css@3.4.12",
+    "core-js": "npm:core-js@1.2.6",
     "css": "github:systemjs/plugin-css@0.1.20",
+    "domain": "github:jspm/nodelibs-domain@0.2.0-alpha",
     "events": "github:jspm/nodelibs-events@0.2.0-alpha",
     "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
     "http": "github:jspm/nodelibs-http@0.2.0-alpha",
@@ -41,18 +43,21 @@ SystemJS.config({
     "net": "github:jspm/nodelibs-net@0.2.0-alpha",
     "os": "github:jspm/nodelibs-os@0.2.0-alpha",
     "path": "github:jspm/nodelibs-path@0.2.0-alpha",
-    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.2",
+    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.5",
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
-    "react": "npm:react@0.14.7",
-    "react-dom": "npm:react-dom@0.14.7",
+    "react": "npm:react@15.0.1",
+    "react-dom": "npm:react-dom@15.0.1",
     "react-dragula": "npm:react-dragula@1.1.15",
     "react-dropzone-component": "npm:react-dropzone-component@1.0.0",
     "sortablejs": "npm:sortablejs@1.4.2",
     "stream": "github:jspm/nodelibs-stream@0.2.0-alpha",
+    "string_decoder": "github:jspm/nodelibs-string_decoder@0.2.0-alpha",
     "systemjs-hot-reloader": "github:capaj/systemjs-hot-reloader@0.5.6",
     "tty": "github:jspm/nodelibs-tty@0.2.0-alpha",
     "url": "github:jspm/nodelibs-url@0.2.0-alpha",
-    "util": "github:jspm/nodelibs-util@0.2.0-alpha"
+    "util": "github:jspm/nodelibs-util@0.2.0-alpha",
+    "whatwg-fetch": "npm:whatwg-fetch@0.11.0",
+    "zlib": "github:jspm/nodelibs-zlib@0.2.0-alpha"
   },
   packages: {
     "github:capaj/systemjs-hot-reloader@0.5.6": {
@@ -62,24 +67,29 @@ SystemJS.config({
         "weakee": "npm:weakee@1.0.0"
       }
     },
-    "github:frankwallis/plugin-typescript@4.0.2": {
+    "github:frankwallis/plugin-typescript@4.0.5": {
       "map": {
         "typescript": "npm:typescript@1.8.9"
       }
     },
     "github:jspm/nodelibs-buffer@0.2.0-alpha": {
       "map": {
-        "buffer-browserify": "npm:buffer@4.5.0"
+        "buffer-browserify": "npm:buffer@4.5.1"
+      }
+    },
+    "github:jspm/nodelibs-domain@0.2.0-alpha": {
+      "map": {
+        "domain-browserify": "npm:domain-browser@1.1.7"
       }
     },
     "github:jspm/nodelibs-http@0.2.0-alpha": {
       "map": {
-        "http-browserify": "npm:stream-http@2.2.0"
+        "http-browserify": "npm:stream-http@2.2.1"
       }
     },
     "github:jspm/nodelibs-os@0.2.0-alpha": {
       "map": {
-        "os-browserify": "npm:os-browserify@0.2.0"
+        "os-browserify": "npm:os-browserify@0.2.1"
       }
     },
     "github:jspm/nodelibs-stream@0.2.0-alpha": {
@@ -87,19 +97,40 @@ SystemJS.config({
         "stream-browserify": "npm:stream-browserify@2.0.1"
       }
     },
+    "github:jspm/nodelibs-string_decoder@0.2.0-alpha": {
+      "map": {
+        "string_decoder-browserify": "npm:string_decoder@0.10.31"
+      }
+    },
     "github:jspm/nodelibs-url@0.2.0-alpha": {
       "map": {
         "url-browserify": "npm:url@0.11.0"
       }
     },
-    "npm:buffer@4.5.0": {
+    "github:jspm/nodelibs-zlib@0.2.0-alpha": {
       "map": {
-        "base64-js": "npm:base64-js@1.1.1",
+        "zlib-browserify": "npm:browserify-zlib@0.1.4"
+      }
+    },
+    "npm:babel-plugin-syntax-flow@6.5.0": {
+      "map": {
+        "babel-runtime": "npm:babel-runtime@5.8.38"
+      }
+    },
+    "npm:browserify-zlib@0.1.4": {
+      "map": {
+        "pako": "npm:pako@0.2.8",
+        "readable-stream": "npm:readable-stream@2.0.6"
+      }
+    },
+    "npm:buffer@4.5.1": {
+      "map": {
+        "base64-js": "npm:base64-js@1.1.2",
         "ieee754": "npm:ieee754@1.1.6",
         "isarray": "npm:isarray@1.0.0"
       }
     },
-    "npm:clean-css@3.4.10": {
+    "npm:clean-css@3.4.12": {
       "map": {
         "commander": "npm:commander@2.8.1",
         "source-map": "npm:source-map@0.4.4"
@@ -132,6 +163,43 @@ SystemJS.config({
         "crossvent": "npm:crossvent@1.5.4"
       }
     },
+    "npm:encoding@0.1.12": {
+      "map": {
+        "iconv-lite": "npm:iconv-lite@0.4.13"
+      }
+    },
+    "npm:fbjs@0.8.0": {
+      "map": {
+        "babel-plugin-syntax-flow": "npm:babel-plugin-syntax-flow@6.5.0",
+        "core-js": "npm:core-js@1.2.6",
+        "isomorphic-fetch": "npm:isomorphic-fetch@2.2.1",
+        "loose-envify": "npm:loose-envify@1.1.0",
+        "promise": "npm:promise@7.1.1",
+        "ua-parser-js": "npm:ua-parser-js@0.7.10"
+      }
+    },
+    "npm:isomorphic-fetch@2.2.1": {
+      "map": {
+        "node-fetch": "npm:node-fetch@1.5.0",
+        "whatwg-fetch": "npm:whatwg-fetch@0.11.0"
+      }
+    },
+    "npm:loose-envify@1.1.0": {
+      "map": {
+        "js-tokens": "npm:js-tokens@1.0.3"
+      }
+    },
+    "npm:node-fetch@1.5.0": {
+      "map": {
+        "encoding": "npm:encoding@0.1.12",
+        "is-stream": "npm:is-stream@1.0.1"
+      }
+    },
+    "npm:promise@7.1.1": {
+      "map": {
+        "asap": "npm:asap@2.0.3"
+      }
+    },
     "npm:react-dragula@1.1.15": {
       "map": {
         "atoa": "npm:atoa@1.0.0",
@@ -143,9 +211,11 @@ SystemJS.config({
         "dropzone": "npm:dropzone@4.3.0"
       }
     },
-    "npm:react@0.14.7": {
+    "npm:react@15.0.1": {
       "map": {
-        "fbjs": "npm:fbjs@0.6.1"
+        "fbjs": "npm:fbjs@0.8.0",
+        "loose-envify": "npm:loose-envify@1.1.0",
+        "object-assign": "npm:object-assign@4.0.1"
       }
     },
     "npm:readable-stream@2.0.6": {
@@ -169,7 +239,7 @@ SystemJS.config({
         "readable-stream": "npm:readable-stream@2.0.6"
       }
     },
-    "npm:stream-http@2.2.0": {
+    "npm:stream-http@2.2.1": {
       "map": {
         "builtin-status-codes": "npm:builtin-status-codes@2.0.0",
         "inherits": "npm:inherits@2.0.1",
