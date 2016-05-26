4267249f48686f1db176583e0c7b8f55f1d64a2d
Piotr Witek <piotrek.witek@gmail.com>
2016-05-26 23:29:30 +0200

    add ts lib array include

---
+++ src/typings/app-typings.d.ts
@@ -0,0 +1,33 @@
+/// <reference path="../../typings/main.d.ts" />
+
+declare module 'fileapi' {
+  const api: any;
+  export = api;
+}
+
+declare module 'mobx' {
+  export const observable: any;
+  export const autorun: any;
+}
+declare module 'mobx-react' {
+  export const observer: any;
+}
+
+declare module 'node-uuid' {
+  export function v1(): string;
+  export function v4(): string;
+}
+
+declare module 'dropzone' {
+  const api: any;
+  export = api;
+}
+
+declare module 'sortablejs' {
+  const api: any;
+  export = api;
+}
+
+declare module 'react-dragula' {
+  export default null;
+}
